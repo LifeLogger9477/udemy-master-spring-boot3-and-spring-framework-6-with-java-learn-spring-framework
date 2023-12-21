00ba7215b5116df3d9bbd18c63c40efbf8293b9d
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2023-12-21 16:42:01 +0900

    섹션 02:Step 09 - Creating More Java Spring Beans in Spring Java Configuration File : 빈 등록방법 - class + record

--- src/main/java/com/in28minutes/learnspringframework/HelloWordConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWordConfiguration.java
@@ -8,6 +8,16 @@ import org.springframework.context.annotation.Configuration;
  * date   : 2023-12-21
  */
 
+// record in JDK 16
+// getter & setter 를 사용할 필요가 없다.
+record Person(String name, int age) {
+};
+
+// address
+record Address(String firstLine, String city) {
+
+};
+
 @Configuration
 public class HelloWordConfiguration {
 
@@ -15,5 +25,23 @@ public class HelloWordConfiguration {
   public String name() {
 
     return "Ranga";
+  }
+
+  @Bean
+  public int age() {
+
+    return 15;
+  }
+
+  @Bean
+  public Person person() {
+
+    return new Person( "Ravi", 20 );
+  }
+
+  @Bean
+  public Address address() {
+
+    return new Address( "Baker Street", "London" );
   }
 }
